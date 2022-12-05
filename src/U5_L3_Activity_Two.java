fd7453050e37db2e74a425a01ad2c16bd3cff1c0
Wesley Hoy <whoy@ey3.tech>
2022-12-05 14:44:36 -0500

    Add activity two

---
+++ src/U5_L3_Activity_Two.java
@@ -0,0 +1,14 @@
+/* Lesson 3 Coding Activity Question 2 */
+
+import java.util.Scanner;
+import shapes.*;
+import testing.Math;
+
+public class U5_L3_Activity_Two
+{
+    public static void randomize(Rectangle rect){
+        rect.setLength((int) (java.lang.Math.random() * 6) *2 + 10);
+        rect.setWidth((int) (java.lang.Math.random() * 4) *2 + 7);
+
+    }
+}
